6773efc696c07ddad8235efe80817db0364023c0
cuong03pl <cuongculebarca@gmail.com>
2025-10-15 14:55:07 +0700

    Xử lý tách candidate table

---
+++ src/components/Candidate/CandidateItem.vue
@@ -0,0 +1,159 @@
+<template>
+  <tr>
+    <td><input type="checkbox" class="row-checkbox" /></td>
+    <td>
+      <Button isOnlyIcon btnDelete>
+        <i class="fa-solid fa-trash-can"></i>
+      </Button>
+    </td>
+    <td>
+      <div class="avatar-cell">
+        <div class="avatar flex items-center justify-center text-secondary">${avatar}</div>
+        <div class="user-info">
+          <div class="user-name">${item.CandidateName}</div>
+        </div>
+      </div>
+    </td>
+    <td class="line-clamp-1">${item.Mobile}</td>
+    <td class="line-clamp-1">${item.Email}</td>
+    <td>--</td>
+    <td class="line-clamp-1">${item.JobPositionName}</td>
+    <td class="line-clamp-1">${item.RecruitmentName}</td>
+    <td><span class="status">${item.RecruitmentRoundName}</span></td>
+    <td>
+      <div class="rating">
+        <svg class="star" viewBox="0 0 24 24">
+          <path
+            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+          />
+        </svg>
+        <svg class="star" viewBox="0 0 24 24">
+          <path
+            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+          />
+        </svg>
+        <svg class="star" viewBox="0 0 24 24">
+          <path
+            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+          />
+        </svg>
+        <svg class="star" viewBox="0 0 24 24">
+          <path
+            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+          />
+        </svg>
+        <svg class="star" viewBox="0 0 24 24">
+          <path
+            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+          />
+        </svg>
+      </div>
+    </td>
+    <td>${applyDate}</td>
+  </tr>
+</template>
+
+<script setup>
+import Button from '../Button/Button.vue'
+</script>
+
+<style scoped>
+td {
+  padding: 12px 16px;
+  font-size: 14px;
+  color: var(--text-color);
+  border-bottom: 1px solid #d5d6d9;
+}
+/* Avatar */
+.avatar-cell {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  flex-shrink: 0;
+}
+
+.avatar.blue {
+  background: var(--btn-primary);
+}
+
+.avatar.yellow {
+  background: #fbbf24;
+}
+
+.avatar.green {
+  background: #4ade80;
+}
+
+.avatar.pink {
+  background: #f472b6;
+}
+
+.avatar.orange {
+  background: #fb923c;
+}
+
+.user-info {
+  display: flex;
+  gap: 4px;
+}
+
+.user-name {
+  font-weight: 500;
+  color: var(--text-color);
+  width: 100px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  display: block;
+}
+
+.user-role {
+  font-size: 12px;
+  color: #10b981;
+  display: flex;
+  align-items: center;
+  gap: 4px;
+}
+
+.badge {
+  background: var(--btn-primary);
+  color: white;
+  padding: 4px 12px;
+  font-weight: 500;
+}
+
+.status {
+  display: inline-block;
+  border-radius: 16px;
+  font-weight: 500;
+}
+
+.rating {
+  display: flex;
+  gap: 2px;
+}
+
+.star {
+  width: 16px;
+  height: 16px;
+  color: var(--border-secondary);
+}
+
+.star.filled {
+  color: #fbbf24;
+  fill: #fbbf24;
+}
+
+input[type='checkbox'] {
+  width: 16px;
+  height: 16px;
+  cursor: pointer;
+}
+</style>
--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -43,51 +43,7 @@
 
       <!-- table -->
       <div class="table mt-5">
-        <div class="table-container">
-          <table>
-            <thead>
-              <tr>
-                <th style="width: 48px">
-                  <input type="checkbox" id="selectAll" />
-                </th>
-                <th></th>
-                <th>Họ và tên</th>
-                <th>Số điện thoại</th>
-                <th>Email</th>
-                <th>Chiến dịch tuyển dụng</th>
-                <th>Vị trí tuyển dụng</th>
-                <th>Tin tuyển dụng</th>
-                <th>Vòng tuyển dụng</th>
-                <th>Đánh giá</th>
-                <th>Ngày ứng tuyển</th>
-              </tr>
-            </thead>
-            <tbody></tbody>
-          </table>
-        </div>
-        <!-- table footer -->
-        <div class="footer px-5 pt-5">
-          <div class="footer-left">Tổng: <span>5381</span> bản ghi</div>
-          <div class="footer-right">
-            <div class="page-size">
-              <span>Số bản ghi/trang</span>
-              <select>
-                <option>25</option>
-                <option>50</option>
-                <option>100</option>
-              </select>
-            </div>
-            <div class="page-size">
-              <span>1 - 25 bản ghi</span>
-            </div>
-            <div class="pagination">
-              <button disabled>
-                <i class="fa-solid fa-chevron-left"></i>
-              </button>
-              <button><i class="fa-solid fa-chevron-right"></i></button>
-            </div>
-          </div>
-        </div>
+        <Candidate />
       </div>
     </div>
   </div>
@@ -96,8 +52,9 @@
 
 <script setup>
 import Button from '@/components/Button/Button.vue'
+import Candidate from '@/components/Candidate/Candidate.vue'
 import CandidateModal from '@/components/Modal/CandidateModal.vue'
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 const isOpen = ref(false)
 const hanleToggleModal = () => {
   isOpen.value = !isOpen.value
@@ -145,214 +102,10 @@ const hanleToggleModal = () => {
 }
 
 /* table */
-.table-container {
-  flex: 1;
-  overflow-y: auto;
-  overflow-x: auto;
-  background: white;
-  max-height: calc(100vh - 310px);
-  height: auto;
-}
 
 table {
   width: 100%;
   border-collapse: collapse;
   background: white;
-}
-.empty-component {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 300px;
-  user-select: none;
-}
-thead {
-  background: #f9fafb;
-  border: 1px solid #e5e7eb;
-  position: sticky;
-  top: -5px;
-  z-index: 10;
-}
-
-th {
-  padding: 12px 16px;
-  text-align: left;
-  font-size: 14px;
-  font-weight: 600;
-  color: var(--text-color);
-  white-space: nowrap;
-}
-
-td {
-  padding: 12px 16px;
-  font-size: 14px;
-  color: var(--text-color);
-  border-bottom: 1px solid #d5d6d9;
-}
-
-tbody tr {
-  transition: background-color 0.15s;
-  cursor: pointer;
-}
-
-tbody tr:hover {
-  background-color: var(--table-item-hover);
-}
-
-/* Avatar */
-.avatar-cell {
-  display: flex;
-  align-items: center;
-  gap: 12px;
-}
-
-.avatar {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  color: white;
-  flex-shrink: 0;
-}
-
-.avatar.blue {
-  background: var(--btn-primary);
-}
-
-.avatar.yellow {
-  background: #fbbf24;
-}
-
-.avatar.green {
-  background: #4ade80;
-}
-
-.avatar.pink {
-  background: #f472b6;
-}
-
-.avatar.orange {
-  background: #fb923c;
-}
-
-.user-info {
-  display: flex;
-  gap: 4px;
-}
-
-.user-name {
-  font-weight: 500;
-  color: var(--text-color);
-  width: 100px;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  display: block;
-}
-
-.user-role {
-  font-size: 12px;
-  color: #10b981;
-  display: flex;
-  align-items: center;
-  gap: 4px;
-}
-
-.badge {
-  background: var(--btn-primary);
-  color: white;
-  padding: 4px 12px;
-  font-weight: 500;
-}
-
-.status {
-  display: inline-block;
-  border-radius: 16px;
-  font-weight: 500;
-}
-
-.rating {
-  display: flex;
-  gap: 2px;
-}
-
-.star {
-  width: 16px;
-  height: 16px;
-  color: var(--border-secondary);
-}
-
-.star.filled {
-  color: #fbbf24;
-  fill: #fbbf24;
-}
-
-input[type='checkbox'] {
-  width: 16px;
-  height: 16px;
-  cursor: pointer;
-}
-
-/* footer table*/
-.table .footer {
-  background: white;
-  border-top: 1px solid #e5e7eb;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.footer-left {
-  font-size: 14px;
-  color: var(--text-color);
-}
-
-.footer-left span {
-  font-weight: 600;
-  color: var(--text-color);
-}
-
-.footer-right {
-  display: flex;
-  align-items: center;
-  gap: 16px;
-}
-
-.page-size {
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  font-size: 14px;
-  color: var(--text-color);
-}
-
-.page-size select {
-  padding: 4px 8px;
-  border: 1px solid var(--border-secondary);
-  border-radius: 4px;
-  font-size: 14px;
-}
-
-.pagination {
-  display: flex;
-  gap: 8px;
-}
-
-.pagination button {
-  padding: 4px 12px;
-  border: 1px solid var(--border-secondary);
-  background: white;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 14px;
-}
-
-.pagination button:hover {
-  background: #f9fafb;
-}
-
-.pagination button:disabled {
-  opacity: 0.5;
-  cursor: not-allowed;
 }
 </style>
